f1395a3aa9bd9bdc502969492e6fd3ed6a09e189
VigeteX <vova.pel.vp@gmail.com>
2025-11-19 19:24:28 +0200

    -

--- configs/wdio.conf.js
+++ configs/wdio.conf.js
@@ -9,26 +9,39 @@ exports.config = {
     ],
 
     maxInstances: 1,
+    // capabilities: [{
+    //     "platformName": "Android",
+    //     "appium:platformVersion": "16",
+    //     "appium:deviceName": "emulator-5554",
+    //     //"appium:deviceName": "Google Pixel 6",
+    //     "appium:automationName": "UiAutomator2",
+    //     "appium:app": "E:/LQ/LQ tc/8/browserstack_test-main/Android-NativeDemoApp-0.4.0.apk",
+    //     //"appium:app": "bs://<APP_UPLOADED_ID>",
+    //     "appium:noReset": false,
+    //     "appium:fullReset": true,
+    //     "appium:appPackage": "com.wdiodemoapp",
+    //     "appium:appActivity": "com.wdiodemoapp.MainActivity",
+    //     "bstack:options": {
+    //         userName: process.env.BROWSERSTACK_USER,
+    //         accessKey: process.env.BROWSERSTACK_KEY,
+    //         projectName: "MyProject",               
+    //         buildName: "Build 1",                   
+    //         sessionName: "Login Test"    
+    //     }
+    // }],
     capabilities: [{
+        "appium:protocol": "https",
+        "appium:hostname": "hub.browserstack.com",
+        "appium:path": "/wd/hub",
+        "appium:maxInstances": 1,
+        "appium:app": "bs://68e5f65b6504b4ded77005d5cc7c12a59a090919",
+        "appium:os_version": "9.0",
+        "appium:deviceName": "Google Pixel 3",
         "platformName": "Android",
-        "appium:platformVersion": "16",
-        //"appium:deviceName": "emulator-5554",
-        "appium:deviceName": "Google Pixel 6",
-        "appium:automationName": "UiAutomator2",
-        // "appium:app": "E:/LQ/LQ tc/8/browserstack_test-main/Android-NativeDemoApp-0.4.0.apk",
-        "appium:app": "bs://<APP_UPLOADED_ID>",
-        "appium:noReset": true,
-        //"appium:fullReset": true,
-        "appium:appPackage": "com.wdiodemoapp",
-        "appium:appActivity": "com.wdiodemoapp.MainActivity",
-        "bstack:options": {
-            userName: process.env.BROWSERSTACK_USER,
-            accessKey: process.env.BROWSERSTACK_KEY,
-            projectName: "MyProject",               
-            buildName: "Build 1",                   
-            sessionName: "Login Test"    
-        }
+        "appium:autoAcceptAlerts": "true",
+        "appium:browserstack.appium_version": "1.22.0"
     }],
+
     logLevel: 'error',
     bail: 0,
     waitforTimeout: 10000,
