b378c99cb850ac2aa7f36efc270dd5dc4a49eace
VigeteX <vova.pel.vp@gmail.com>
2025-11-22 01:03:01 +0200

    Update wdio.conf.js

--- configs/wdio.conf.js
+++ configs/wdio.conf.js
@@ -1,73 +1,53 @@
 
-// exports.config = {
-//     runner: 'local',
-//     userName: "viget_p5C1Ef",
-//     accessKey: "JcttkD6YqvbwYc9xpCxP",
-//     protocol: 'https',
-//     hostname: 'hub.browserstack.com',
-//     port: 443,
-//     path: '/wd/hub',
+exports.config = {
+    runner: 'local',
+    userName: "viget_p5C1Ef",
+    accessKey: "JcttkD6YqvbwYc9xpCxP",
+    protocol: 'https',
+    hostname: 'hub.browserstack.com',
+    //port: 443,
+    path: '/wd/hub',
 
-//     // port: 4723,
-//     specs: [
-//         '../test/specs/**/*.js'
-//     ],
-//     exclude: [
-//     ],
+     \port: 4723,
+    specs: [
+        '../test/specs/**/*.js'
+    ],
+    exclude: [
+    ],
 
-//     maxInstances: 1,
-//     capabilities: [{
-//         "platformName": "Android",
-//         "appium:platformVersion": "16",
-//         //"appium:deviceName": "emulator-5554",
-//         "appium:deviceName": "Google Pixel 6",
-//         "appium:automationName": "UiAutomator2",
-//         // "appium:app": "E:/LQ/LQ tc/8/browserstack_test-main/Android-NativeDemoApp-0.4.0.apk",
-//         "appium:app": "bs://<APP_UPLOADED_ID>",
-//         "appium:noReset": true,
-//         //"appium:fullReset": true,
-//         "appium:appPackage": "com.wdiodemoapp",
-//         "appium:appActivity": "com.wdiodemoapp.MainActivity",
-//         "bstack:options": {
-//             // userName: process.env.BROWSERSTACK_USERNAME,
-//             // accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
-            
-//             projectName: "MyProject",               
-//             buildName: "Build 1",                   
-//             sessionName: "Login Test"    
-//         }
-//     }],
-//     logLevel: 'error',
-//     bail: 0,
-//     waitforTimeout: 10000,
-//     connectionRetryTimeout: 120000,
-//     connectionRetryCount: 3,
-//     //services: ['appium'],
-//     services: ['browserstack'],
-//     framework: 'mocha',
-//     reporters: ['spec'],
-//     mochaOpts: {
-//         ui: 'bdd',
-//         timeout: 60000
-//     },
-// }
-export const config = {
-    user: 'viget_p5C1Ef',
-    key: 'JcttkD6YqvbwYc9xpCxP',
-    services: ['browserstack'],
+    maxInstances: 1,
     capabilities: [{
-        platformName: 'Android',
-        'appium:platformVersion': '16',
-        'appium:deviceName': 'Google Pixel 6',
-        'appium:automationName': 'UiAutomator2',
-        'appium:app': 'bs://<APP_UPLOADED_ID>',
-        'appium:noReset': true,
-        'appium:appPackage': 'com.wdiodemoapp',
-        'appium:appActivity': 'com.wdiodemoapp.MainActivity',
-        'bstack:options': {
-            projectName: 'MyProject',
-            buildName: 'Build 1',
-            sessionName: 'Login Test'
+        "platformName": "Android",
+        "appium:platformVersion": "16",
+        //"appium:deviceName": "emulator-5554",
+        "appium:deviceName": "Google Pixel 6",
+        "appium:automationName": "UiAutomator2",
+        // "appium:app": "E:/LQ/LQ tc/8/browserstack_test-main/Android-NativeDemoApp-0.4.0.apk",
+        "appium:app": "bs://<APP_UPLOADED_ID>",
+        "appium:noReset": true,
+        //"appium:fullReset": true,
+        "appium:appPackage": "com.wdiodemoapp",
+        "appium:appActivity": "com.wdiodemoapp.MainActivity",
+        "bstack:options": {
+            // userName: process.env.BROWSERSTACK_USERNAME,
+            // accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
+            
+            projectName: "MyProject",               
+            buildName: "Build 1",                   
+            sessionName: "Login Test"    
         }
-    }]
-};
+    }],
+    logLevel: 'error',
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    //services: ['appium'],
+    services: ['browserstack'],
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}
