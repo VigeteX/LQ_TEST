2e3c9b83ee8388f2a8087a6189f4f7ca0fd2ca07
VigeteX <vova.pel.vp@gmail.com>
2025-11-22 21:33:27 +0200

    Update wdio.conf.js

--- configs/wdio.conf.js
+++ configs/wdio.conf.js
@@ -1,10 +1,13 @@
 
 exports.config = {
     runner: 'local',
-    user: 'viget_p5C1Ef',       // BrowserStack username
-    key: 'JcttkD6YqvbwYc9xpCxP', // BrowserStack access key
-    userName: "viget_p5C1Ef",
-    accessKey: "JcttkD6YqvbwYc9xpCxP",
+    // user: 'viget_p5C1Ef',       // BrowserStack username
+    // key: 'JcttkD6YqvbwYc9xpCxP', // BrowserStack access key
+    user: process.env.BROWSERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_ACCESS_KEY,
+
+    //userName: "viget_p5C1Ef",
+    //accessKey: "JcttkD6YqvbwYc9xpCxP",
     protocol: 'https',
     hostname: 'hub.browserstack.com',
     port: 443,
@@ -19,17 +22,22 @@ exports.config = {
 
     maxInstances: 1,
     capabilities: [{
+        //"appium:deviceName": "emulator-5554",
+        //"appium:fullReset": true,
+        // "appium:app": "E:/LQ/LQ tc/8/browserstack_test-main/Android-NativeDemoApp-0.4.0.apk",
         "platformName": "Android",
         "appium:platformVersion": "16",
-        //"appium:deviceName": "emulator-5554",
         "appium:deviceName": "Google Pixel 6",
         "appium:automationName": "UiAutomator2",
-        // "appium:app": "E:/LQ/LQ tc/8/browserstack_test-main/Android-NativeDemoApp-0.4.0.apk",
         "appium:app": "bs://68e5f65b6504b4ded77005d5cc7c12a59a090919",
         "appium:noReset": true,
-        //"appium:fullReset": true,
         "appium:appPackage": "com.wdiodemoapp",
         "appium:appActivity": "com.wdiodemoapp.MainActivity",
+        "bstack:options": {
+            projectName: "MyProject",
+            buildName: "Build 1",
+            sessionName: "Login Test"
+        }
         // "bstack:options": {
         //     // userName: process.env.BROWSERSTACK_USERNAME,
         //     // accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
@@ -44,8 +52,9 @@ exports.config = {
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    //services: ['appium'],
-    services: ['browserstack'],
+
+    services: ['appium','browserstack'],
+    
     framework: 'mocha',
     reporters: ['spec'],
     mochaOpts: {
